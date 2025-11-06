17a571e4dcc168b2bffdfcb2e3dcca55d33fe503
Amanda Midttun Søreide <amanda.soreide@gmail.com>
2025-11-06 09:55:17 +0100

    Added api and Http trigger HttpRecipes

---
+++ api/HttpRecipes.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api;
+
+public class HttpRecipes
+{
+    private readonly ILogger<HttpRecipes> _logger;
+
+    public HttpRecipes(ILogger<HttpRecipes> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpRecipes")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
